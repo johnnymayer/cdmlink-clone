a1ed619f8e3d823a7531207b5063208d6abf2975
Johnny Mayer <johnnymayer@gmail.com>
2018-03-30 09:24:20 -0700

    Frame out blogpost model, start more framing for blog posting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { BlogPost } from './models/BlogPost.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  masterBlogPostList: BlogPost[] = [
+    new BlogPost('Blog Post 1', 'https://images.unsplash.com/photo-1503440765842-0c8293cb78ba?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=d4c12f73e77bae7703247a6641cc03c3&auto=format&fit=crop&w=1349&q=80', 'lorem ipsum text here'),
+    new BlogPost('Blog Post 2', 'https://images.unsplash.com/photo-1474314243412-cd4a79f02c6a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f2c3e328eaa43749d28bc198dafcbfb3&auto=format&fit=crop&w=1351&q=80', 'lorem ipsum text here'),
+    new BlogPost('Blog Post 3', 'https://images.unsplash.com/photo-1466767574486-40e61f77bdd6?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=42afb2e7f389e92324b9f2917182b3ed&auto=format&fit=crop&w=1350&q=80', 'lorem ipsum text here')
+  ]
 }
--- src/app/models/blogPost.model.ts
+++ src/app/models/blogPost.model.ts
@@ -1,8 +1,10 @@
 export class BlogPost {
   constructor (
-    public title: string;
-    public image: string;
-    // public image: string, should be URL for image?  how will writers create blog posts, share images?//
-    public post: string;
+    public title: string,
+    public image: string,
+    public post: string
   ) {}
 }
+
+
+// public image: string, should be URL for image?  how will writers create blog posts, share images?//
