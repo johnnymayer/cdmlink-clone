c47811e98c6c6913a3337ccea447b0a54b521fe7
Johnny Mayer <johnnymayer@gmail.com>
2018-03-30 15:03:30 -0700

    limitTo pipe not found, in process to resolve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { TruncatePipe } from './app.pipe';
 import { AppComponent } from './app.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { BlogPostListComponent } from './blog-post-list/blog-post-list.component';
+import { TruncatePipe } from './app.pipe';
 import { LimitToPipe } from './limit-to.pipe';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
