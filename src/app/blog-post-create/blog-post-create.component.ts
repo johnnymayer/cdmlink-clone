36bedd48420d13ad70f5217096368ed8efb0c889
Johnny Mayer <johnnymayer@gmail.com>
2018-03-30 15:55:28 -0700

    Add blog post html, next step integrate to button

--- src/app/blog-post-create/blog-post-create.component.ts
+++ src/app/blog-post-create/blog-post-create.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { BlogPost } from '../models/BlogPost.model';
 
 @Component({
   selector: 'app-blog-post-create',
   templateUrl: './blog-post-create.component.html',
   styleUrls: ['./blog-post-create.component.css']
 })
-export class BlogPostCreateComponent implements OnInit {
+export class BlogPostCreateComponent {
 
-  constructor() { }
+  @Input() childBlogPostToAdd: boolean;
+  @Output() clickedAdd = new EventEmitter();
 
-  ngOnInit() {
+  addBlogPost = null;
+
+  newBlogPost(title: string, image: string, post: string,) {
+    let newBlogPost: BlogPost = new BlogPost(title, image, post);
+    console.log(newBlogPost);
+    this.clickedAdd.emit(newBlogPost);
+    
+
   }
 
 }
