0d80a3e017c7cf89897c67dbf8ce6fe27d836a3a
Johnny Mayer <johnnymayer@gmail.com>
2018-03-30 15:32:39 -0700

    Add blog-post-create component

---
+++ src/app/blog-post-create/blog-post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-create',
+  templateUrl: './blog-post-create.component.html',
+  styleUrls: ['./blog-post-create.component.css']
+})
+export class BlogPostCreateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
