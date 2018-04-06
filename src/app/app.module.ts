a3e3e8c28afc56401b18e6970653740fe4296ad9
Johnny Mayer <johnnymayer@gmail.com>
2018-04-06 09:11:31 -0700

    Delete blog-post-create component (This is a proposal feature).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
+import { masterFirebaseConfig } from './api-keys';
+import { routing } from './app.routing';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { BlogPostListComponent } from './blog-post-list/blog-post-list.component';
-import { Pipe, PipeTransform } from '@angular/core';
-import { LimitToPipe } from './limit-to.pipe';
-import { HeaderComponent } from './header/header.component';
-import {  } from './app.routing';
 import { ArchiveComponent } from './archive/archive.component';
-import { masterFirebaseConfig } from './api-keys';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { HttpModule } from '@angular/http';
+import { HeaderComponent } from './header/header.component';
+import { LimitToPipe } from './limit-to.pipe';
+
+
 
 export const firebaseConfig = {
   apiKey: masterFirebaseConfig.apiKey,
@@ -32,8 +36,11 @@ export const firebaseConfig = {
   imports: [
     BrowserModule,
     HttpModule,
+    routing,
+    RouterModule.forRoot(appRoutes),
     AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule
+    AngularFireDatabaseModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
